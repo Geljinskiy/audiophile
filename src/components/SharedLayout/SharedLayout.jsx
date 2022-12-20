5dccca62eae8b8b7988827ff8fa535c69818b3e1
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-20 13:34:29 +0200

    Added fonts to the project

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 const SharedLayout = () => {
   return (
     <>
-      <div>Header is here</div>
+      <div style={{ fontFamily: 'ManropeMedium' }}>Header is here</div>
       <Outlet />
-      <div>Footer is here</div>
+      <div style={{ fontFamily: 'ManropeRegular' }}>Footer is here</div>
     </>
   );
 };
