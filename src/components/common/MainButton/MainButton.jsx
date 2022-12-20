7cd94b49bf04a7ffed42646f877dba7f23aff9db
Geljinskiy <savvageljinskiy@gmail.com>
2022-12-20 13:48:32 -0500

    added prop types, added button type, fixed theh path of colors file

--- src/components/common/MainButton/MainButton.jsx
+++ src/components/common/MainButton/MainButton.jsx
@@ -1,12 +1,17 @@
 import styles from './MianButton.module.scss';
+import PropTypes from 'prop-types';
 
-const MainButton = ({ btnStyle, children }) => {
+const MainButton = ({ btnStyle, btnType="button", children }) => {
   return (
-    <button className={`${styles.button} ${styles[btnStyle]}`}>
+    <button type={btnType} className={`${styles.button} ${styles[btnStyle]}`}>
       {children}
     </button>
   );
 };
 
+MainButton.propTypes = {
+  btnStyle: PropTypes.string.isRequired,
+  btnType: PropTypes.string,
+};
 
 export default MainButton;
