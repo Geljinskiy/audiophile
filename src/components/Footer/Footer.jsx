b0aad51a8f31641428680fa0c9a9133b4e99eae7
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-22 00:40:37 +0200

    Improved footer items order using flex instead of relative positioning

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,9 @@
-import { Container, FooterLinks, FooterSocialLinks } from 'components';
+import {
+  Container,
+  FooterLinks,
+  FooterSocialLinks,
+  FooterBodyText,
+} from 'components';
 
 import css from './Footer.module.scss';
 
@@ -7,16 +12,8 @@ const Footer = () => {
     <footer className={css.footer}>
       <Container className={css.footer__container}>
         <FooterLinks />
-        <p className={`${css.footer__about_us} ${css.footer__text}`}>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a
-          small team of music lovers and sound specialists who are devoted to
-          helping you get the most out of personal audio. Come and visit our
-          demo facility - we’re open 7 days a week.
-        </p>
-        <div className={css.footer__bottom_wrapper}>
-          <p className={`${css.footer__copyright} ${css.footer__text}`}>
-            Copyright 2021. All Rights Reserved
-          </p>
+        <div className={css.footer__wrapper}>
+          <FooterBodyText />
           <FooterSocialLinks />
         </div>
       </Container>
