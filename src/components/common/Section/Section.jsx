4bec1592d988e9f606745074dca550685ccfd07f
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-22 22:57:11 +0200

    Added comments for Section component

--- src/components/common/Section/Section.jsx
+++ src/components/common/Section/Section.jsx
@@ -1,5 +1,15 @@
 import css from './Section.module.scss';
 
+/**
+ * React Component which returns children wrapped in a section tag
+ *
+ * @param {JSX.Element} children - JSX Element
+ * @param {!string} className - scss class with additional styles
+ *
+ * @example
+ *
+ * <Section className={string}>{children}</Section>
+ */
 const Section = ({ children, className }) => {
   return (
     <section className={`${css.section} ${className ?? ''}`}>
