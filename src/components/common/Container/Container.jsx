29df8b01cd03691681ad8d8ccbce6cf994fa6254
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-22 22:59:51 +0200

    Added comments for Container component

--- src/components/common/Container/Container.jsx
+++ src/components/common/Container/Container.jsx
@@ -1,5 +1,13 @@
 import css from './Container.module.scss';
 
+/**
+ * A React Component which returns children in a styled wrapped
+ *
+ * @param {string} className - scss class with additional styling
+ * @param {JSX.Element} children - JSX Element
+ *
+ * @example <Container className={string}>{children}</Container>
+ */
 const Container = ({ children, className }) => {
   return (
     <div className={`${css.container} ${className ?? ''}`}>{children}</div>
