71b4f114e2cb54f048818d9431faa57a18b09b0f
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-22 21:13:02 +0200

    Added images assets for Products Reviews sections with index path importing

--- src/assets/images/mobile/index.js
+++ src/assets/images/mobile/index.js
@@ -1,1 +1,4 @@
 export { default as bestGearMobileImg } from './best-gear-mobile.jpg';
+export { default as EarphonesYX1MobileImg } from './earphones-yx1-mobile.jpg';
+export { default as SpeakerZX7MobileImg } from './speaker-zx7-mobile.jpg';
+export { default as SpeakerZX9MobileImg } from './speaker-zx9-mobile.png';
--- src/assets/images/tablet/index.js
+++ src/assets/images/tablet/index.js
@@ -1,1 +1,4 @@
 export { default as bestGearTabletImg } from './best-gear-tablet.jpg';
+export { default as EarphonesYX1TabletImg } from './earphones-yx1-tablet.jpg';
+export { default as SpeakerZX7TabletImg } from './speaker-zx7-tablet.jpg';
+export { default as SpeakerZX9TabletImg } from './speaker-zx9-tablet.png';
