4347fd2767b6050d26fcd78e451f4b5a5374761b
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-25 16:58:13 +0200

    Added opacity for transparent button; completed styling categories links section for all media versions; added categories thumbnail images to assets

--- src/components/section/CategoriesLinks/CategoriesLinks.jsx
+++ src/components/section/CategoriesLinks/CategoriesLinks.jsx
@@ -2,35 +2,63 @@ import { Link } from 'react-router-dom';
 
 import { Button, Section } from 'components';
 
+import { ROUTES } from 'utils';
+
 import css from './CategoriesLinks.module.scss';
-import { ArrowRightIcon } from 'assets';
+import {
+  ArrowRightIcon,
+  CategoryEarphonesImg,
+  CategoryHeadphonesImg,
+  CategorySpeakersImg,
+} from 'assets';
 
 const CategoriesLinks = () => {
   return (
-    <Section>
-      <div className={`${css.category} ${css.category__mb}`}>
-        <h3 className={css.category_title}>Headphones</h3>
-        <Button className={css.button} styling="transparent">
-          <Link className={css.link}>
-            Shop <ArrowRightIcon />
-          </Link>
-        </Button>
+    <Section className={css.section}>
+      <div className={css.category_img_wrapper}>
+        <img
+          className={`${css.category_img} ${css.category_img__headphones}`}
+          src={CategoryHeadphonesImg}
+          alt="Headphones"
+        />
+        <div className={css.category}>
+          <h3 className={css.category_title}>Headphones</h3>
+          <Button className={css.button} styling="transparent">
+            <Link to={ROUTES.headphones} className={css.link}>
+              Shop <ArrowRightIcon />
+            </Link>
+          </Button>
+        </div>
       </div>
-      <div className={`${css.category} ${css.category__mb}`}>
-        <h3 className={css.category_title}>Speakers</h3>
-        <Button className={css.button} styling="transparent">
-          <Link className={css.link}>
-            Shop <ArrowRightIcon />
-          </Link>
-        </Button>
+      <div className={css.category_img_wrapper}>
+        <img
+          className={`${css.category_img} ${css.category_img__speakers}`}
+          src={CategorySpeakersImg}
+          alt="Speakers"
+        />
+        <div className={`${css.category} ${css.category__mb}`}>
+          <h3 className={css.category_title}>Speakers</h3>
+          <Button className={css.button} styling="transparent">
+            <Link to={ROUTES.speakers} className={css.link}>
+              Shop <ArrowRightIcon />
+            </Link>
+          </Button>
+        </div>
       </div>
-      <div className={`${css.category}`}>
-        <h3 className={css.category_title}>Earphones</h3>
-        <Button className={css.button} styling="transparent">
-          <Link className={css.link}>
-            Shop <ArrowRightIcon />
-          </Link>
-        </Button>
+      <div className={css.category_img_wrapper}>
+        <img
+          className={`${css.category_img} ${css.category_img__earphones}`}
+          src={CategoryEarphonesImg}
+          alt="Earphones"
+        />
+        <div className={`${css.category}`}>
+          <h3 className={css.category_title}>Earphones</h3>
+          <Button className={css.button} styling="transparent">
+            <Link to={ROUTES.earphones} className={css.link}>
+              Shop <ArrowRightIcon />
+            </Link>
+          </Button>
+        </div>
       </div>
     </Section>
   );
