ec686572fefa6053ce8d82c04785714b97943435
savelii <savvageljinskiy@gmail.com>
2023-06-19 18:28:57 -0400

    feat: svg files
    Add svg file for header into assets dir.
    
    Task: https://todoist.com/app/project/2304123564/task/6450174101"

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,5 @@ export { ReactComponent as FacebookIcon } from './facebook.svg';
 export { ReactComponent as Logo } from './logo.svg';
 export { ReactComponent as InstagramIcon } from './instagram.svg';
 export { ReactComponent as TwitterIcon } from './twitter.svg';
+export { ReactComponent as BurgerIcon } from './burger.svg';
+export { ReactComponent as CartIcon } from './cart.svg';
