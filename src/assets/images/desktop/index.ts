7dfd2f84dc332906125a5d087c8b9a734404a02a
savelii <savvageljinskiy@gmail.com>
2023-06-20 23:15:01 -0400

    feat: images&icons
    Task: https://todoist.com/app/project/2304123564/task/6450175504

--- src/assets/images/desktop/index.ts
+++ src/assets/images/desktop/index.ts
@@ -2,3 +2,6 @@ export { default as bestGearDesktopImg } from './best-gear-desktop.jpg';
 export { default as EarphonesYX1DesktopImg } from './earphones-yx1-desktop.jpg';
 export { default as SpeakerZX7DesktopImg } from './speaker-zx7-desktop.jpg';
 export { default as SpeakerZX9DesktopImg } from './speaker-zx9-desktop.png';
+export { default as SpeakersDesktopImg } from './speakers.png';
+export { default as HeadphonesDesktopImg } from './headphones.png';
+export { default as EarphonesDesktopImg } from './earphones.png';
