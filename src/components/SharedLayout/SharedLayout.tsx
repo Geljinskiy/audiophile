249656b97a6816840300312959be0f5cc3ae41e3
savelii <savvageljinskiy@gmail.com>
2023-06-30 18:13:42 -0400

    bg for chekcout page

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -4,17 +4,16 @@ import { useLocation } from 'react-router-dom';
 // local imports
 import { Outlet } from 'react-router-dom';
 import { Header, Footer } from 'components';
-import { ROUTES } from 'utils';
-import css from './SharedLayout.module.scss'
+import css from './SharedLayout.module.scss';
 
 const SharedLayout: React.FC = (): JSX.Element => {
   const loc = useLocation();
   const isHomePage = loc.pathname === '/';
-
+  const isCheckoutPage = loc.pathname === '/checkout';
   return (
     <>
       <Header type={isHomePage ? 'transparent' : 'solid'} />
-      <main className={css}>
+      <main className={isCheckoutPage && css.checkoutBg}>
         <Outlet />
       </main>
       <Footer />
